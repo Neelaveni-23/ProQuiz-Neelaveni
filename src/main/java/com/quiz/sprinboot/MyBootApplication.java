41d6fbdab165fa404fea8c8f5b7180b6c5640419
Uma Maheshwar Rao G <g.umamahesh2@gmail.com>
2025-06-16 18:27:17 +0530

    added the review comments

--- src/main/java/com/quiz/sprinboot/MyBootApplication.java
+++ src/main/java/com/quiz/sprinboot/MyBootApplication.java
@@ -10,3 +10,12 @@ public class MyBootApplication {
 		SpringApplication.run(MyBootApplication.class, args);
 	}
 }
+
+
+//com.quiz.sprinboot.controller -> all ur controllers
+//com.quiz.sprinboot.service -> all ur service
+//com.quiz.sprinboot.dao -> all ur daos
+//implement swagger
+//update the java <!--properties-->
+// add some background color to homepage
+// after login display some message. login successfully done. dash board etc.
